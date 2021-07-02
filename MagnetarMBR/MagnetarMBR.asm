231acf132c24877b94659b68ff38664a20719dc4
Niko <nikoharianto58@gmail.com>
2021-07-02 12:19:49 +0700

    Add files via upload

---
+++ MagnetarMBR/MagnetarMBR.asm
@@ -0,0 +1,27 @@
+;crap mbr lol
+
+BITS 16
+ORG 0x7C00
+
+    xor al, al
+    xor bx, bx
+    mov cx, 0x01
+	
+    rainbow:
+    mov ah, 0x09
+    int 10h
+    mov ah, 0x0e
+    int 10h
+	
+	inc al
+	inc bl
+	inc cl
+	
+	inc ah
+	inc bh
+	inc ch
+
+    jmp rainbow
+ 
+times 510-($-$$) db 0
+dw 0xAA55
