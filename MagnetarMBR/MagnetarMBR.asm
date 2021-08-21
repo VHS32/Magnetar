07e6abd9616fd916f1e8733c50d65c736e553e14
Nathan <nikoharianto58@gmail.com>
2021-08-21 11:07:08 +0700

    Comments

--- MagnetarMBR/MagnetarMBR.asm
+++ MagnetarMBR/MagnetarMBR.asm
@@ -1,16 +1,20 @@
-bits 16
-org 0x7c00
+bits 16 ; Bootloader using 16 bits.
+org 0x7c00 ; BIOS magic number.
 
+;Clear screen + set position.
     xor al, al
     xor bx, bx
     mov cx, 0x01
 	
-    rainbow:
+repeat: ;repeat entry point.
+
+    ; Print color and txt.
     mov ah, 0x09
     int 0x10
     mov ah, 0x0e
     int 0x10
-
+    
+    ; Incrementing some register to generate random character and color (i little forgot about that).
     inc al
     inc bl
     inc cl
@@ -19,7 +23,8 @@ org 0x7c00
     inc bh
     inc ch
 
-    jmp rainbow
+    ; Jump again to repeat entry point (loops).
+    jmp repeat
  
-times 510-($-$$) db 0
-dw 0xAA55
+times 510-($-$$) db 0 ; Fill until reach 510b.
+dw 0xAA55 ; 2b mbr signature.
